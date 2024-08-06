24e574d4002b9af0a44ff7c04f8329c8c9e1828a
shsw <sae.sahatsawat@gmail.com>
2024-08-06 13:03:42 +0700

    Change module name

---
+++ src/conf/mdmr_config.go
@@ -0,0 +1,31 @@
+package mdmr_config
+
+	type Configurations struct {
+		Server		ServerConfigurations
+		Database	DatabaseConfigurations
+		Logger		LoggerConfigurations
+		MDMR	SoftwareConfigurations
+	}
+
+	type ServerConfigurations struct {
+		// SourceAddress: "ipaddress1:port1, ipaddress2:port2,..."
+		SourceAddress string
+		// DestinationAddress: "ipaddress:port"
+		DestinationAddress string
+	}
+
+	type DatabaseConfigurations struct {
+		SourceDBUser string
+		SourceDBPassword string
+		DestinationDBUser string
+		DestinationDBPassword string
+	}
+
+	type LoggerConfigurations struct {
+		LogDirectory string
+		LogFileName string
+	}
+
+	type SoftwareConfigurations struct {
+		Threads int
+	}
