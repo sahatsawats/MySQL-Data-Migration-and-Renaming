034c2674cdc32d7c38a06e4daf375f7f10263e6d
shsw <sae.sahatsawat@gmail.com>
2024-08-08 21:13:57 +0700

    Fix logging on repair & Add test case match on OCI

--- src/services/repairDumpHandler.go
+++ src/services/repairDumpHandler.go
@@ -11,78 +11,81 @@ import (
 )
 
 type RepairHandler struct {
-	repairLog		*lumberjack.Logger	
-	repairChan		chan models.RepairTask
-	done			chan struct{}
+	log              *log.Logger
+	repairChan       chan models.RepairTask
+	done             chan struct{}
 	repairStagingDir string
 }
 
-//TODO: Construct the RepairHandler
+// TODO: Construct the RepairHandler
 func NewRepairHandler(logFilePath string, stagingDir string, bufferSize int) *RepairHandler {
 	// Create a logger instance
 	repairLogger := &lumberjack.Logger{
-		Filename: logFilePath,
-		MaxSize: 0,
+		Filename:   logFilePath,
+		MaxSize:    10,
 		MaxBackups: 0,
-		MaxAge: 0,
+		MaxAge:     28,
 	}
+
+	stdLogger := log.New(repairLogger, "RepairHandles: ", log.LstdFlags)
 
 	// Create RepairHandler object
 	repairObject := &RepairHandler{
-		repairLog: repairLogger,
-		repairChan: make(chan models.RepairTask, bufferSize),
-		done: make(chan struct{}),
+		log:              stdLogger,
+		repairChan:       make(chan models.RepairTask, bufferSize),
+		done:             make(chan struct{}),
 		repairStagingDir: stagingDir,
 	}
 
 	// Initialize the RepairObject
 	go repairObject.run()
-	
+
 	// Return the object
 	return repairObject
 }
 
-// TODO: Start run the process. 
+// TODO: Start run the process.
 func (r *RepairHandler) run() {
 	// Infinity loop until received signal
 	for {
 		select {
-		case repairTask := <- r.repairChan:
-			log.Printf("Starting repair task from database: %s", repairTask.DatabaseName)
+		case repairTask := <-r.repairChan:
+			r.log.Printf("Starting repair task from database: %s", repairTask.DatabaseName)
 			// <database_name>-staging-repair
 			stagingFileName := fmt.Sprintf("%s-staging-repair", repairTask.DatabaseName)
 			// Map the staging file name with path
 			stagingPath := filepath.Join(r.repairStagingDir, stagingFileName)
 			cmd := exec.Command(
-				"mysqlsh", "-h", repairTask.MySQLCredentials.Host, "-P", repairTask.MySQLCredentials.Port, 
-				"-u", repairTask.MySQLCredentials.User, 
-				fmt.Sprintf("-p'%s'", repairTask.MySQLCredentials.Password), 
+				"mysqlsh", "-h", repairTask.MySQLCredentials.Host, "-P", repairTask.MySQLCredentials.Port,
+				"-u", repairTask.MySQLCredentials.User,
+				fmt.Sprintf("-p'%s'", repairTask.MySQLCredentials.Password),
 				"-e", fmt.Sprintf("util.dumpSchemas(['%s'], '%s', {threads: 4})", repairTask.DatabaseName, stagingPath))
 
 			err := cmd.Run()
 			if err != nil {
-				log.Printf("Failed to retry database name: %s with error: %v \n", repairTask.DatabaseName, err)
+				r.log.Printf("Failed to retry database name: %s with error: %v \n", repairTask.DatabaseName, err)
+			} else {
+				r.log.Printf("Completed retry database name: %s from %s \n", repairTask.DatabaseName, repairTask.MySQLCredentials.Host)
 			}
-			log.Printf("Completed retry database name: %s from %s \n", repairTask.DatabaseName, repairTask.MySQLCredentials.Host)
-		case <- r.done:
+		case <-r.done:
 			return
 		}
 	}
 }
 
-//TODO: Act as API for send the repairing request to object
+// TODO: Act as API for send the repairing request to object
 func (r *RepairHandler) Repair(databaseName string, credentials models.MySQLCredentials) {
-	log.Printf("Receiving repair task: %s from %s \n", databaseName, credentials.Host)
+	r.log.Printf("Receiving repair task: %s from %s \n", databaseName, credentials.Host)
 	// Create a repairTask with databaseName and Credentails
 	repairTask := models.RepairTask{
-		DatabaseName: databaseName,
+		DatabaseName:     databaseName,
 		MySQLCredentials: credentials,
 	}
 	// Send the repairTask to channel
 	r.repairChan <- repairTask
 }
 
-//TODO: Close the buffer channel
+// TODO: Close the buffer channel
 func (r *RepairHandler) Close() {
 	close(r.done)
 	close(r.repairChan)
---
+++ src/services/repairDumpHandler_test.go
@@ -0,0 +1,74 @@
+package services_test
+
+import (
+	"MDMR/src/models"
+	"MDMR/src/services"
+	"path/filepath"
+	"testing"
+	"os"
+	"time"
+)
+
+
+
+func TestRepairHandler(t *testing.T) {
+	// Temporary directory for testing
+	tmpDir := t.TempDir()
+	//tmpDir := "/Users/sahatsawat/Projects/MDMR/test/services/repairDumpHandler"
+	logFilePath := filepath.Join(tmpDir, "repair.log")
+
+	// Create a RepairHandler
+	repairHandler := services.NewRepairHandler(logFilePath, tmpDir, 10)
+	defer repairHandler.Close()
+
+	// Mock MySQL credentails
+	mockCredentials := models.MySQLCredentials{
+		Host:	"213.35.99.8",
+		Port:	"3306",
+		User: 	"root",
+		Password: "M@Tc#2024",
+	}
+
+	// Create a mock RepairTask
+	mockTask := models.RepairTask{
+		DatabaseName:    "admin_mtls_biz_002",
+		MySQLCredentials: mockCredentials,
+	}
+
+	// Channel to capture the output
+	done := make(chan bool)
+
+	go func() {
+		repairHandler.Repair(mockTask.DatabaseName, mockTask.MySQLCredentials)
+		done <- true
+	}()
+	
+	select {
+	case <-done:
+		time.Sleep(5000 * time.Millisecond)
+
+		_, err := os.Stat(logFilePath)
+		// Check if the log file was created
+		if os.IsNotExist(err) {
+			t.Fatalf("Expected log file at %s but it does not exist", logFilePath)
+		}
+
+
+		expectedDirectory := filepath.Join(tmpDir,"admin_mtls_biz_002-staging_repair")
+
+		if !isDirectoryExists(expectedDirectory){
+			t.Errorf("Error from Match function: %s", err)
+		}
+		
+	case <-time.After(2 * time.Second):
+		t.Fatalf("Test timed out waiting for repair to be processed")
+	}
+}
+
+
+// Helper function to check if a string is contained in another string
+func isDirectoryExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+
+	return os.IsNotExist(err)
+}
